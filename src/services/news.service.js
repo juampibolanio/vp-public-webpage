d0141a23fa8fe8f3203ce296d5de54689096453a
JuampiBolanio <juampi360786@gmail.com>
2026-02-03 11:53:33 -0300

    feat: agregar servicios para traer noticias relacionadas y por slug

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -1,6 +1,10 @@
 import { apiFetch } from "./api";
-import { normalizeNewsResponse } from "../utils/news-normalizer";
+import {
+  normalizeNewsItem,
+  normalizeNewsResponse,
+} from "../utils/news-normalizer";
 
+/* get all news (list news) */
 export async function getNews({ page = 1, pageSize = 5 } = {}) {
   const query = new URLSearchParams({
     "fields[0]": "title",
@@ -22,4 +26,53 @@ export async function getNews({ page = 1, pageSize = 5 } = {}) {
   const response = await apiFetch(`/news?${query.toString()}`);
 
   return normalizeNewsResponse(response);
+}
+
+/* get new by slug (just a news) */
+export async function getNewBySlug(slug) {
+  const query = new URLSearchParams({
+    "filters[slug][$eq]": slug,
+
+    "populate[media]": "true",
+    "populate[author][populate][avatar]": "true",
+  });
+
+  const response = await apiFetch(`/news?${query.toString()}`);
+
+  if (!response?.data?.length) {
+    return null;
+  }
+
+  return normalizeNewsItem(response.data[0]);
+}
+
+/* get a list of related news */
+export async function getRelatedNews({
+  category,
+  excludeSlug,
+  limit = 3,
+}) {
+  const query = new URLSearchParams({
+    "filters[slug][$ne]": excludeSlug,
+
+    ...(category && {
+      "filters[category][$eq]": category.label,
+    }),
+
+    "pagination[pageSize]": limit,
+
+    "fields[0]": "title",
+    "fields[1]": "slug",
+    "fields[2]": "category",
+
+    "populate[media]": "true",
+  });
+
+  const response = await apiFetch(`/news?${query.toString()}`);
+
+  if (!response?.data?.length) {
+    return [];
+  }
+
+  return response.data.map(normalizeNewsItem);
 }
