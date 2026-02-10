d1daeb10b9e59c7c8150ca88b4b0523359f165a4
Manuel0803 <manulautaro2003@gmail.com>
2026-02-09 21:19:47 -0300

    feature: se implemento sección mapa

---
+++ src/services/places.service.js
@@ -0,0 +1,20 @@
+import { apiFetch } from './api';
+import { normalizePlaces } from '../utils/places-normalizer.js';
+
+export async function getPlaces() {
+    try {
+        const endpoint = '/places?filters[is_active][$eq]=true&populate=*';
+        const response = await apiFetch(endpoint);
+        const rawData = response.data || [];
+        const normalizedData = normalizePlaces(rawData);
+
+        return { data: normalizedData, error: null };
+
+    } catch (error) {
+        console.error("[Places Service Error]:", error);
+        return { 
+            data: [], 
+            error: "No se pudieron cargar los lugares." 
+        };
+    }
+}
