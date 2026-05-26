0ca6c776f85ff3f5c773c7d0f905be6c73d13a62
JuampiBolanio <juampi360786@gmail.com>
2026-05-26 11:26:39 -0300

    fix: instalar adaptador para cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import cloudflare from '@astrojs/cloudflare';
 import icon from 'astro-icon';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), react()],
   output: "server",
-  adapter: netlify(),
+  adapter: cloudflare(),
 });
