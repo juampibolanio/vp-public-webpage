e566b26e3bb89c555ff20a955f4289c1b0431eef
JuampiBolanio <juampi360786@gmail.com>
2026-05-27 20:49:45 -0300

    chore: agregar adapter para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel/serverless';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [icon(), react()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
